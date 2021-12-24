6bcef0b73a722fd321e99b1e8cae66ba9e7c2ce3
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-12-24 00:56:51 -0300

    Seletor de tema implementado

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -12,7 +12,7 @@ var App = (function () {
 
     self.set_theme = function (theme_name) {
         var theme = "/assets/css/theme/" + theme_name + ".css";
-        $('link[title="theme-css"]').attr("href", theme);
+        $('link[href*="assets/css/theme"]').attr("href", theme);
 
         self.theme = theme_name;
         let supports_storage = supports_html5_storage();
@@ -21,6 +21,18 @@ var App = (function () {
             localStorage.theme = theme_name;
         }
     };
+
+    self.set_font = function (font_name) {
+        var font = "/assets/css/font/" + font_name + ".css";
+        $('link[href*="assets/css/font"]').attr("href", font);
+
+        self.font = font_name;
+        let supports_storage = supports_html5_storage();
+
+        if (supports_storage) {
+            localStorage.font = font_name;
+        }
+    }
 
     self.init_theme = function () {
         let supports_storage = supports_html5_storage();
@@ -34,6 +46,21 @@ var App = (function () {
         } else {
             set_theme("darkly");
             $("#theme-chooser").hide();
+        }
+    };
+
+    self.init_font = function () {
+        let supports_storage = supports_html5_storage();
+        if (supports_storage) {
+            var font_name = localStorage.font;
+            if (font_name) {
+                set_font(font_name);
+            } else {
+                set_font("sans");
+            }
+        } else {
+            set_font("sans");
+            $("#font-chooser").hide();
         }
     };
 
@@ -51,9 +78,25 @@ var App = (function () {
         });
     };
 
+    self.initFontSelector = function () {
+        $(':radio[name="fontChoice"]').click(function () {
+            let font_name = $(this).val();
+            set_font(font_name);
+        });
+
+        let settingsModal = document.getElementById('settingsModal')
+
+        settingsModal.addEventListener("show.bs.modal", function (event) {
+            let font_name = self.font;
+            $("#font_" + font_name).prop("checked", true);
+        });
+    };
+
     self.init = function () {
         self.initThemeSelector();
+        self.initFontSelector()
         self.init_theme();
+        self.init_font();
     };
 
     return {
